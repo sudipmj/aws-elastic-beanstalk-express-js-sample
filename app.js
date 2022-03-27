e021d1b1c04518a6166b2a767135fcf2a2dbae42
SUDIP MAJI <sdpmj2688g@gmail.com>
2022-03-27 18:41:21 +0530

    "Full Pipeline"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from sample app of SDP!'));
+app.get('/', (req, res) => res.send('Hello from sample app of SDP New Full Pipeline Test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
